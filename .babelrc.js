cf113aae49019319797c2a1584ff9971565097f3
lowlandjuju <everin.m.scott@gmail.com>
2021-04-13 17:09:01 +0200

    Remove node_modules in imported module to avoid conflicts

--- .babelrc.js
+++ .babelrc.js
@@ -1,5 +1,5 @@
 'use strict';
 module.exports = {
-  presets: [['@babel/env', { loose: true, modules: 'auto' }], '@babel/react'],
+  presets: [['@babel/env', { loose: true, modules: false }], '@babel/react'],
   plugins: [['transform-react-remove-prop-types', { mode: 'remove' }]],
 };
